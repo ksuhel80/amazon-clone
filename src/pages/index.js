02be284509ccec5d4496552a4a4a230328153faa
ksuhel801 <suhel@vicdigit-technologies.com>
2022-10-30 12:26:08 +0530

    Changed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,25 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import ProductFeed from '../components/ProductFeed'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home( { products } )  {
+export default function Home({ products }) {
   return (
     <div className="">
       <Head>
-        <title>Amazon Clone</title>
+        <title>Amazon Clone 1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
       <Header></Header>
 
       <main className="max-w-screen-2xl mx-auto">
-
         <Banner />
 
         <ProductFeed products={products} />
-
       </main>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
@@ -30,8 +27,8 @@ export async function getServerSideProps(context) {
     (res) => res.json()
   );
   return {
-    props:{
-      products
-    }
-  }
+    props: {
+      products,
+    },
+  };
 }
